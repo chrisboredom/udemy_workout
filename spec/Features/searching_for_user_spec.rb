762b7a2488c914aa2cdc8b3c04e0f8b4ccf82266
Chris Mangano <chris.mangano@me.com>
2016-12-17 15:24:36 -0500

    Implement searching

--- spec/Features/searching_for_user_spec.rb
+++ spec/Features/searching_for_user_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
-RSpec.feature "Listing Members" do
+RSpec.feature "Searching for User" do
   before do
     @john = User.create(first_name: "John", last_name: "Doe", email: "johndoe@example.com",
       password: "password")
-    @sarah = User.create(first_name: "Sarah", last_name: "Joseph", email: "sarah@example.com",
+    @sarah = User.create(first_name: "Sarah", last_name: "Doe", email: "sarah@example.com",
       password: "password")
   end
 
-  scenario "shows a list of registered members" do
+  scenario "with existing name returns all those users" do
     visit "/"
-    expect(page).to have_content("List of Members")
+
+    fill_in "search_name", with: "Doe"
+    click_button "Search"
+
     expect(page).to have_content(@john.full_name)
     expect(page).to have_content(@sarah.full_name)
+    expect(current_path).to eq("/dashboard/search")
   end
 end
